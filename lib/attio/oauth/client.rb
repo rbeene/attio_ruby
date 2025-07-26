866a7c06d8441805e8662ed19ebbf9c1f5aa9a2a
Robert Beene <robert@ismly.com>
2025-07-26 09:19:12 -0400

    feat: implement OAuth 2.0 authentication
    - Create OAuth Client with authorization URL builder
    - Implement authorization code exchange flow
    - Add token refresh and revocation support
    - Create Token class with expiration tracking
    - Implement comprehensive scope validator
    - Add token introspection support
    - Include secure token storage interface

---
+++ lib/attio/oauth/client.rb
@@ -0,0 +1,174 @@
+# frozen_string_literal: true
+
+require "uri"
+require "securerandom"
+require "base64"
+
+module Attio
+  module OAuth
+    class Client
+      OAUTH_BASE_URL = "https://app.attio.com/authorize"
+      TOKEN_URL = "https://api.attio.com/v2/oauth/token"
+      DEFAULT_SCOPES = %w[
+        record:read
+        record:write
+        object:read
+        object:write
+        list:read
+        list:write
+        webhook:read
+        webhook:write
+        user:read
+      ].freeze
+
+      attr_reader :client_id, :client_secret, :redirect_uri
+
+      def initialize(client_id:, client_secret:, redirect_uri:)
+        @client_id = client_id
+        @client_secret = client_secret
+        @redirect_uri = redirect_uri
+        @connection_manager = Util::ConnectionManager.new
+        validate_config!
+      end
+
+      # Generate authorization URL for OAuth flow
+      def authorization_url(scopes: DEFAULT_SCOPES, state: nil, extras: {})
+        state ||= generate_state
+        scopes = validate_scopes(scopes)
+
+        params = {
+          client_id: client_id,
+          redirect_uri: redirect_uri,
+          response_type: "code",
+          scope: scopes.join(" "),
+          state: state
+        }.merge(extras)
+
+        uri = URI.parse(OAUTH_BASE_URL)
+        uri.query = URI.encode_www_form(params)
+        
+        {
+          url: uri.to_s,
+          state: state
+        }
+      end
+
+      # Exchange authorization code for access token
+      def exchange_code_for_token(code:, state: nil)
+        raise ArgumentError, "Authorization code is required" if code.nil? || code.empty?
+
+        params = {
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: redirect_uri,
+          client_id: client_id,
+          client_secret: client_secret
+        }
+
+        response = make_token_request(params)
+        Token.new(response.merge(client: self))
+      end
+
+      # Refresh an existing access token
+      def refresh_token(refresh_token)
+        raise ArgumentError, "Refresh token is required" if refresh_token.nil? || refresh_token.empty?
+
+        params = {
+          grant_type: "refresh_token",
+          refresh_token: refresh_token,
+          client_id: client_id,
+          client_secret: client_secret
+        }
+
+        response = make_token_request(params)
+        Token.new(response.merge(client: self))
+      end
+
+      # Revoke a token
+      def revoke_token(token)
+        token_value = token.is_a?(Token) ? token.access_token : token
+        
+        params = {
+          token: token_value,
+          client_id: client_id,
+          client_secret: client_secret
+        }
+
+        request = Util::RequestBuilder.build(
+          method: :POST,
+          path: "/oauth/revoke",
+          params: params
+        )
+
+        @connection_manager.execute(request)
+        true
+      rescue Errors::Base
+        # Token might already be revoked
+        false
+      end
+
+      # Validate token with introspection endpoint
+      def introspect_token(token)
+        token_value = token.is_a?(Token) ? token.access_token : token
+        
+        params = {
+          token: token_value,
+          client_id: client_id,
+          client_secret: client_secret
+        }
+
+        request = Util::RequestBuilder.build(
+          method: :POST,
+          path: "/oauth/introspect",
+          params: params
+        )
+
+        response = @connection_manager.execute(request)
+        Util::ResponseParser.parse(response)
+      end
+
+      private
+
+      def validate_config!
+        raise ArgumentError, "client_id is required" if client_id.nil? || client_id.empty?
+        raise ArgumentError, "client_secret is required" if client_secret.nil? || client_secret.empty?
+        raise ArgumentError, "redirect_uri is required" if redirect_uri.nil? || redirect_uri.empty?
+        
+        unless redirect_uri.start_with?("http://", "https://")
+          raise ArgumentError, "redirect_uri must be a valid HTTP(S) URL"
+        end
+      end
+
+      def validate_scopes(scopes)
+        scopes = Array(scopes).map(&:to_s)
+        return DEFAULT_SCOPES if scopes.empty?
+        
+        invalid_scopes = scopes - ScopeValidator::VALID_SCOPES
+        unless invalid_scopes.empty?
+          raise ArgumentError, "Invalid scopes: #{invalid_scopes.join(', ')}"
+        end
+        
+        scopes
+      end
+
+      def generate_state
+        SecureRandom.urlsafe_base64(32)
+      end
+
+      def make_token_request(params)
+        request = {
+          method: :POST,
+          uri: URI.parse(TOKEN_URL),
+          headers: {
+            "Content-Type" => "application/x-www-form-urlencoded",
+            "Accept" => "application/json"
+          },
+          body: URI.encode_www_form(params)
+        }
+
+        response = @connection_manager.execute(request)
+        Util::ResponseParser.parse(response)
+      end
+    end
+  end
+end
---
+++ lib/attio/oauth/token.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+module Attio
+  module OAuth
+    class Token
+      attr_reader :access_token, :refresh_token, :token_type, :expires_in,
+                  :expires_at, :scope, :created_at, :client
+
+      def initialize(attributes = {})
+        @access_token = attributes[:access_token] || attributes["access_token"]
+        @refresh_token = attributes[:refresh_token] || attributes["refresh_token"]
+        @token_type = attributes[:token_type] || attributes["token_type"] || "Bearer"
+        @expires_in = (attributes[:expires_in] || attributes["expires_in"])&.to_i
+        @scope = parse_scope(attributes[:scope] || attributes["scope"])
+        @created_at = attributes[:created_at] || Time.now.utc
+        @client = attributes[:client]
+        
+        calculate_expiration!
+        validate!
+      end
+
+      def expired?
+        return false if @expires_at.nil?
+        Time.now.utc >= @expires_at
+      end
+
+      def expires_soon?(threshold = 300)
+        return false if @expires_at.nil?
+        Time.now.utc >= (@expires_at - threshold)
+      end
+
+      def refresh!
+        raise InvalidTokenError, "No refresh token available" unless @refresh_token
+        raise InvalidTokenError, "No OAuth client configured" unless @client
+        
+        new_token = @client.refresh_token(@refresh_token)
+        update_from(new_token)
+        self
+      end
+
+      def revoke!
+        raise InvalidTokenError, "No OAuth client configured" unless @client
+        
+        @client.revoke_token(self)
+        @access_token = nil
+        @refresh_token = nil
+        true
+      end
+
+      def to_h
+        {
+          access_token: @access_token,
+          refresh_token: @refresh_token,
+          token_type: @token_type,
+          expires_in: @expires_in,
+          expires_at: @expires_at&.iso8601,
+          scope: @scope,
+          created_at: @created_at.iso8601
+        }.compact
+      end
+
+      def to_json(*args)
+        JSON.generate(to_h, *args)
+      end
+
+      def inspect
+        "#<#{self.class.name}:#{object_id} " \
+          "token=#{@access_token ? '***' + @access_token[-4..] : 'nil'} " \
+          "expires_at=#{@expires_at&.iso8601} " \
+          "scope=#{@scope.join(' ')}>"
+      end
+
+      # Authorization header value
+      def authorization_header
+        "#{@token_type} #{@access_token}"
+      end
+
+      # Check if token has specific scope
+      def has_scope?(scope)
+        @scope.include?(scope.to_s)
+      end
+
+      # Store token securely (subclasses can override)
+      def save
+        # Default implementation does nothing
+        # Subclasses can implement secure storage
+        self
+      end
+
+      # Load token from secure storage (class method)
+      def self.load(identifier = nil)
+        # Default implementation returns nil
+        # Subclasses can implement secure retrieval
+        nil
+      end
+
+      private
+
+      def calculate_expiration!
+        if @expires_in
+          @expires_at = @created_at + @expires_in
+        else
+          @expires_at = nil
+        end
+      end
+
+      def parse_scope(scope)
+        case scope
+        when String
+          scope.split(" ")
+        when Array
+          scope.map(&:to_s)
+        else
+          []
+        end
+      end
+
+      def validate!
+        raise InvalidTokenError, "Access token is required" if @access_token.nil? || @access_token.empty?
+        raise InvalidTokenError, "Invalid token type" unless %w[Bearer bearer].include?(@token_type)
+      end
+
+      def update_from(other_token)
+        @access_token = other_token.access_token
+        @refresh_token = other_token.refresh_token if other_token.refresh_token
+        @token_type = other_token.token_type
+        @expires_in = other_token.expires_in
+        @expires_at = other_token.expires_at
+        @scope = other_token.scope
+        @created_at = other_token.created_at
+      end
+
+      class InvalidTokenError < StandardError; end
+    end
+  end
+end
